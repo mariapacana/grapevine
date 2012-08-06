fa12980941a60c3c7d2cac724803a645002808a9
Maria Pacana <maria.pacana@gmail.com>
2012-08-05 19:58:33 -0700

    For *odd* turns, generates an ERB template to email.

--- cgi-bin/xhrtest.rb
+++ cgi-bin/xhrtest.rb
@@ -28,7 +28,7 @@ end
 
 def new(data, email)
 	db = SQLite3::Database.new("picdata.db")
-	turn= 1
+	turn = 1
 	optedout = 0
 	
 	db.execute("insert into games (turn) values (?)", turn)
@@ -37,16 +37,23 @@ def new(data, email)
 	db.execute("insert into pics (data, gameid, turn) values (?,?,?)", [data, gameid, turn]) 
 	
 	for e in email do
-		if db.execute("select * from players where email = ?",e).empty?
+		playerid = nil
+		r = db.execute("select playerid from players where email = ?",e)
+		
+		if !r.empty?
+			playerid = r[0][0]
+		else
 			db.execute("insert into players (email, optedout) values (?,?)", [e,optedout])
+			playerid = db.last_insert_row_id().to_i
 		end
 	
-		playerid = db.last_insert_row_id().to_i
 		token = rand(10000)
 		params = [gameid, playerid, turn, token]
 		db.execute("insert into gamestoplayers (gameid, playerid, turn, token) values (?,?,?,?)", params) 
 		turn = turn + 1
 	end
+	
+	gameid
 end
 
 def show(gameid,turn)
@@ -55,11 +62,11 @@ def show(gameid,turn)
 	maxturns = db.execute("select count(*) from gamestoplayers where gameid=?",gameid)[0][0]+1
 	
 	#shows pics
-	if (turn.odd?) && (turn < maxturns)
+	if turn.odd? && turn < maxturns
 		picdata = db.execute("select data from pics where gameid = ? and turn = ?", [gameid,turn])[0][0]
 		template_data = IO.read('cgi-bin/templates/picturn.html.erb')
   	template = ERB.new(template_data)
-  elsif (turn.even?) && (turn < maxturns)
+  elsif turn.even? && turn < maxturns
   #shows sentences
 		sentence = db.execute("select sentence from sentences where gameid = ? and turn = ?", [gameid,turn])[0][0]
 		template_data = IO.read('cgi-bin/templates/senturn.html.erb')
@@ -90,6 +97,35 @@ def show(gameid,turn)
   
 end
 
+def send_email(gameid,turn)
+	db = SQLite3::Database.new("picdata.db")
+	maxturns = db.execute("select count(*) from gamestoplayers where gameid=?",gameid)[0][0]+1
+	email_message = ''
+	
+  # look up game and turn to find next player and turn type
+  # load appropriate template and fill in blanks
+  # there will be 3 types of template (pic, sentence, final) & text & html versions.
+  if turn.odd? && turn < maxturns
+		currentplayerid = db.execute("select playerid from gamestoplayers where gameid = ? and turn = ?", [gameid,turn])[0][0]
+		nextplayerid = db.execute("select playerid from gamestoplayers where gameid = ? and turn = ?", [gameid,turn+1])[0][0]
+		
+		currentplayere = db.execute("select email from players where playerid = ?", currentplayerid)[0][0]
+		nextplayere = db.execute("select email from players where playerid = ?", nextplayerid)[0][0]
+
+		template_data = IO.read('cgi-bin/templates/picturnemail.txt.erb')
+  	template = ERB.new(template_data)
+  	email_message = template.result(binding)
+  elsif turn.even? && turn < maxturns
+  else
+  end
+  
+  File.open('sentemail.txt', 'w') do |file|
+  	file.puts email_message
+  end
+  
+  # create tmail message
+end
+
 def savesentence(sentence,gameid,turn)
 	db = SQLite3::Database.new("picdata.db")
 	db.execute("insert into sentences (sentence, gameid, turn) values (?,?,?)", [sentence, gameid, turn]) 
@@ -108,8 +144,8 @@ def main
   end
 	cmd = $params["cmd"][0]
 	
-	if (cmd == "new") #Creates a new game
-		data = URI.unescape($params["data"][0]).to_s #changes &&s for instance
+	if (cmd == "new") # Creates a new game
+		data = URI.unescape($params["data"][0]).to_s # changes &&s for instance
 		
 		if ($params["email"][0] == "") 
 			error("Type a valid email address.")
@@ -117,22 +153,25 @@ def main
 			email = $params["email"][0].split(",")
 		end
 		
-		for i in email do i.strip! end
-		new(data, email)	
-	elsif (cmd == "show") #Shows the current turn
+		email.each {|i| i.strip! }
+		gameid = new(data, email)	
+		send_email(gameid, 1)
+	elsif (cmd == "show") # Shows the current turn
 		gameid = $params["gameid"][0]
 		turn = $params["turn"][0].to_i
 		show(gameid, turn) #INCLUDE TOKENS LATER
-	elsif (cmd == "sentence") #Updates database with a sentence
+	elsif (cmd == "sentence") # Updates database with a sentence
 		sentence = URI.unescape($params["sentence"][0]).to_s
 		gameid = $params["gameid"][0]
 		turn = $params["turn"][0].to_i+1
 		savesentence(sentence,gameid,turn)
-	elsif (cmd == "pic") #Updates database with a picture
+		send_email(gameid, turn)
+	elsif (cmd == "pic") # Updates database with a picture
 		data = URI.unescape($params["data"][0]).to_s
 		gameid = $params["gameid"][0]
 		turn = $params["turn"][0].to_i+1
 		savepic(data,gameid,turn)
+		send_email(gameid, turn)
 	end
 end
 
